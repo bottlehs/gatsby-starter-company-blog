bd1fab3f6a6fcad409a2720cdaf3f0db803b53dd
bottlehs <qudgns9@gmail.com>
2021-04-27 01:09:49 +0900

    헤더추가

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,47 +30,55 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div>
-      <div className="top">
-        <ul className="navbar">
-          <li>
-            <Link className="" to="/">
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link className="" to="/about">
-              About
-            </Link>
-          </li>
-        </ul>
-
-        <div className="theme">
-          <ThemeToggler>
-            {({ theme, toggleTheme }) => (
-              <Switch
-                onChange={(e) =>
-                  toggleTheme(theme === 'dark' ? 'light' : 'dark')
-                }
-                checked={theme === 'dark'}
-                offColor="#b2dcf2"
-                onColor="#423090"
-                uncheckedIcon={
-                  <IconContext.Provider
-                    value={{ color: '#f4da87', className: 'theme-sun-icon' }}
-                  >
-                    <RiSunFill />
-                  </IconContext.Provider>
-                }
-                checkedIcon={
-                  <IconContext.Provider
-                    value={{ color: '#e7eee5', className: 'theme-moon-icon' }}
-                  >
-                    <RiMoonClearFill />
-                  </IconContext.Provider>
-                }
-              />
-            )}
-          </ThemeToggler>
+      <div className="navbar">
+        <div className="navbar-container">
+          <div className="logo">
+            <a href="">
+              <img src="/static/b0059e14d98bced5c806c552ae09b998/318fd/profile-pic.png"></img>
+            </a>
+          </div>
+          <div className="menu">
+            <ul>
+              <li>
+                <Link className="" to="/">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link className="" to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="theme">
+            <ThemeToggler>
+              {({ theme, toggleTheme }) => (
+                <Switch
+                  onChange={(e) =>
+                    toggleTheme(theme === 'dark' ? 'light' : 'dark')
+                  }
+                  checked={theme === 'dark'}
+                  offColor="#b2dcf2"
+                  onColor="#423090"
+                  uncheckedIcon={
+                    <IconContext.Provider
+                      value={{ color: '#f4da87', className: 'theme-sun-icon' }}
+                    >
+                      <RiSunFill />
+                    </IconContext.Provider>
+                  }
+                  checkedIcon={
+                    <IconContext.Provider
+                      value={{ color: '#e7eee5', className: 'theme-moon-icon' }}
+                    >
+                      <RiMoonClearFill />
+                    </IconContext.Provider>
+                  }
+                />
+              )}
+            </ThemeToggler>
+          </div>
         </div>
       </div>
       <div className="global-wrapper" data-is-root-path={isRootPath}>
