adc69c2cc7a4ddb7ed63326c4a6bd1a85e580f51
bottlehs <qudgns9@gmail.com>
2021-05-04 10:05:29 +0900

    tags

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -15,6 +15,7 @@ const TagsPage = ({ data, location }) => {
     return (
       <Layout location={location} title={siteTitle}>
         <SEO title="All tags" />
+        <Bio />
         <p>
           No blog posts found. Add markdown posts to "content/posts" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
@@ -27,48 +28,34 @@ const TagsPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle + " Tags"}>
       <SEO title="All tags" />
+      <Bio />
       <ol style={{ listStyle: `none` }}>
         {tags.map(tag => {
           return (
-            <li key={post.fields.slug}>
+            <li key={tag.fieldValue}>
               <article
-                className="post-list-item"
+                className="tag-list-item"
                 itemScope
                 itemType="http://schema.org/Article"
               >
                 <header>
                   <h2>
-                    <Link to={post.fields.slug} itemProp="url" title={title}>
-                      <span itemProp="headline">{title}</span>
+                    <Link
+                      to={`/tags/${kebabCase(tag.fieldValue)}/`}
+                      itemProp="url"
+                      title={tag.fieldValue}
+                    >
+                      <span itemProp="headline">{tag.fieldValue}</span>
                     </Link>
                   </h2>
-                  <small>{post.frontmatter.date}</small>
                 </header>
                 <section>
-                  {post.frontmatter.thumbnail && (
-                    <div className="post-list-thumbnail">
-                      <img src={post.frontmatter.thumbnail} alt="" />
-                    </div>
-                  )}
-                  <div className="post-list-content">
-                    <p
-                      dangerouslySetInnerHTML={{
-                        __html: post.frontmatter.description || post.excerpt,
-                      }}
-                      itemProp="description"
-                    />
-                    {post.frontmatter.tags && (
-                      <ul>
-                        {post.frontmatter.tags.map((tag) => {
-                          return (
-                            <li key={tag}>
-                              <a href={`/tags/${kebabCase(tag)}/`}>{tag}</a>
-                            </li>
-                          )
-                        })}
-                      </ul>
-                    )}
-                  </div>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: tag.totalCount + " posts" || 0,
+                    }}
+                    itemProp="count"
+                  />
                 </section>
               </article>
             </li>
